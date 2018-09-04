2c6c4e999c4cc1c0655bfe8ff08029b7a1ba9d97
Diego Borrero <die.9511@gmail.com>
2018-09-04 17:20:52 -0500

    Part 3

--- src/App.js
+++ src/App.js
@@ -2,20 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {items: [{text:"Learn React", priority:5, dueDate: new Date() },
+    {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
+    {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }], text: '', priority:0, dueDate:moment()};
+  }
+
   render() {
+    const todos = [{text:"Learn React", priority:5, dueDate: new Date() },
+          {text:"Learn about APIs", priority:4, dueDate: new Date(2018,8,30) },
+          {text:"write TODO App", priority:3, dueDate: new Date(2018,9,30) }];
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>TODO React App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/>
+        <br/>
+        <div>
+          <form>
+            <h2> New Todo </h2>
+            <input id='text' onChange={this.handleChangeText} value={this.state.text} ></input>
+            <input id='priority' onChange={this.handleChangePriority} value={this.state.priority} ></input>
+            <input id='dueDate' onChange={this.handleChangeDate} value={this.state.dueDate} ></input>
+            <button>
+              Add #{this.state.items.length + 1}
+            </button>
+          </form>
+        <br/>
+        </div>
+        <TodoList todoList={todos}/>
       </div>
     );
   }
+
+  handleChangeText(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleChangePriority(e) {
+    this.setState({ priority: e.target.value });
+  }
+
+  handleChangeDate(e) {
+    this.setState({ dueDate: date(e.target.value) });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length || !this.state.priority.length || !this.state.dueDate) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      priority:this.state.priority,
+      dueDate: this.state.dueDate
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: '',
+      priority: '',
+      dueDate: ''
+    }));
+  }
+
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Todo} from './Todo';
+
+class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const todoList = this.props.todoList.map((todo,i) =>{
+            return (
+                <Todo key={i} 
+                text={todo.text} 
+                priority={todo.priority}
+                dueDate={todo.dueDate}/>
+            );
+        });
+
+        return (
+            <table>
+                <tr>
+                    <th>Task</th>
+                    <th>Priority</th>
+                    <th>Due Date</th>
+                </tr>
+                <tbody>
+                    {todoList}
+                </tbody>
+            </table>            
+        );
+    }
+}
